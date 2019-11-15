22f3f567f1723c2a623845a65f5f19c280c5a73a
Nicole Lucien <nicolemlucien@gmail.com>
2019-11-15 16:54:58 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,14 +1,25 @@
 require 'open-uri'
+require 'nokogiri'
 require 'pry'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
+    html = open("https://learn-co-curriculum.github.io/student-scraper-test-page/index.html")
     
+    index_page = Nokogiri::HTML(html)
+    
+    student_index_array = []
+    
+    index_page.css("div.roster-cards-container").collect do |students|
+      student_index_array << {:name => students.css("div.student-name").text, :location => students.css("p.student-location").text, :profile_url => students.xpath("div.view-profile-div/a/@href")}
+     
+    end
+    student_index_array
   end
 
   def self.scrape_profile_page(profile_url)
-    
+    scraped_student = Nokogiri::HTML(open(profile_url))
   end
 
 end
